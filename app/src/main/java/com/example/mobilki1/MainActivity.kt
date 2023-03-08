27bce09a9e99fee6f9051a166ccf4dceac70bf08
VanillaChoc <savanin.maksim@gmail.com>
2023-03-08 20:41:37 +0700

    #2 - интро

--- app/src/main/java/com/example/mobilki1/MainActivity.kt
+++ app/src/main/java/com/example/mobilki1/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.mobilki1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash_screen)
+
+        supportActionBar?.hide()
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity( Intent( this, IntroActivity::class.java ) )
+            finish()
+        }, 2000 )
     }
 }
